47c3a2e0650ec99b896aefbaa68ef2f4050c63ba
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-04-11 22:29:26 +0300

    Add javascript validation code and changes made on index.html on gender

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-console.log( "lets code");
-function submitButton (){
-      console.log ("submit");
-      event.preventDefault();
-
-   
-}
-
-document.getElementById("myCheckbox").addEventListener("click", function(event){
-    event.preventDefault()
-  });
+function dateFunction(){
+      var inputedDate = validateDate();
+      
+  function validateDate(){
+      var date = document.getElementById("Date").value;
+      if (date) {
+          if (date>=1 && date<=31) {
+              return date;
+          }else{
+              alert("date should be 1-31");
+          }
+      }else{
+          alert("please enter a valid date")
+      }
+  }
