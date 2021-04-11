cbb424555aa5f75ee20e6fd154ef50afc85a2175
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-04-11 22:40:48 +0300

    Added validation changes to index.js. The validation is for month and year functions.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function dateFunction(){
       var inputedDate = validateDate();
-      
-  function validateDate(){
+      var inputtedMonth= validateMonth();
+      var inputtedYear= validateYear();
+ 
+      function validateDate(){
       var date = document.getElementById("Date").value;
       if (date) {
           if (date>=1 && date<=31) {
@@ -11,5 +13,29 @@ function dateFunction(){
           }
       }else{
           alert("please enter a valid date")
+      }
+  }
+}
+function validateMonth(){
+    var month = document.getElementById("Month").value;
+    if (month) {
+        if (month>=1 && month<=12) {
+            return month;
+        }else{
+            alert("month should be 1-12");
+        }
+    }else{
+        alert("please enter a valid month")
+    }
+    function validateYear(){
+      var year = document.getElementById("Year").value;
+      if (year) {
+          if (year>=1000 && year<=9999) {
+              return year;
+          }else{
+              alert("year should be a four digit in the format xxxx");
+          }
+      }else{
+          alert("please enter year")
       }
   }
