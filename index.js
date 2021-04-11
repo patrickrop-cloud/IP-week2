e75b3406fe38cfefc3540005b0a4e997f3d02eeb
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-04-11 18:53:59 +0300

    Adding column and list to the index.html page

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log( "lets code");
+function submitButton (){
+      console.log ("submit");
+      event.preventDefault();
+
+   
+}
+
+document.getElementById("myCheckbox").addEventListener("click", function(event){
+    event.preventDefault()
+  });
